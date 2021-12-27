cec9f57b8b2ccd965ac2beb70612ddb6b91807e3
abousow123 <abousow798@gmail.com>
2021-12-27 23:10:00 +0000

    first commit

---
+++ Previ1/src/main/java/org/sid/Previ1Application.java
@@ -0,0 +1,13 @@
+package org.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Previ1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Previ1Application.class, args);
+	}
+
+}
